a1d8128efcb0b4b15547aeac4233512d14e0742a
Kirk Peterson <necrobious@gmail.com>
2012-08-27 09:04:43 -0700

    added LibPQ typeclass

--- Database/PostgreSQL/LibPQTypes.hs
+++ Database/PostgreSQL/LibPQTypes.hs
@@ -1,6 +1,7 @@
 module Database.PostgreSQL.LibPQTypes where
 
 import Database.PostgreSQL.LibPQ
+import Data.ByteString (ByteString)
 
 -- postgres types, defined in from pg_type.h
 
@@ -11,410 +12,418 @@ type values defined in pg_type.h from the PostgreSQL source code.
 future versions of this code should probably extract the CInt value of the type using the FFI, rather than hard coded values here.
 -}
 
-class PgOid oidtype where
-  pgoid :: oidtype -> Oid 
+-- A Typeclass for marking a type as having an oid-type value within libpq.
+-- We use this typeclass to mark types 1:1 for types found in the pg_type.h and pg_type table. 
+class PqOid oidtype where
+  pqoid :: oidtype -> Oid 
+
+-- A Typeclass for marking a type a having the ability to be transformed into and out of LibPQ values.
+class LibPQ a where
+  oidPQ  :: a -> Oid 
+  toPQ   :: a -> Maybe (ByteString, Format)
+  fromPQ :: Maybe ByteString -> Maybe a
 
 -- BOOLOID
 data BOOL = BOOL
-instance PgOid BOOL where
-  pgoid BOOL   = Oid 16
+instance PqOid BOOL where
+  pqoid BOOL   = Oid 16
 
 -- BYTEAOID
 data BYTEA = BYTEA
-instance PgOid BYTEA where
-  pgoid BYTEA   = Oid 17
+instance PqOid BYTEA where
+  pqoid BYTEA   = Oid 17
 
 -- CHAROID
 data CHAR = CHAR
-instance PgOid CHAR where
-  pgoid CHAR   = Oid 18
+instance PqOid CHAR where
+  pqoid CHAR   = Oid 18
 
 -- NAMEOID
 data NAME = NAME
-instance PgOid NAME where
-  pgoid NAME   = Oid 19
+instance PqOid NAME where
+  pqoid NAME   = Oid 19
 
 -- INT8OID
 data INT8 = INT8
-instance PgOid INT8 where
-  pgoid INT8   = Oid 20
+instance PqOid INT8 where
+  pqoid INT8   = Oid 20
 
 -- INT2OID
 data INT2 = INT2
-instance PgOid INT2 where
-  pgoid INT2   = Oid 21
+instance PqOid INT2 where
+  pqoid INT2   = Oid 21
 
 -- INT2VECTOROID
 data INT2VECTOR = INT2VECTOR
-instance PgOid INT2VECTOR where
-  pgoid INT2VECTOR   = Oid 22
+instance PqOid INT2VECTOR where
+  pqoid INT2VECTOR   = Oid 22
 
 -- INT4OID
 data INT4 = INT4
-instance PgOid INT4 where
-  pgoid INT4   = Oid 23
+instance PqOid INT4 where
+  pqoid INT4   = Oid 23
 
 -- REGPROCOID
 data REGPROC = REGPROC
-instance PgOid REGPROC where
-  pgoid REGPROC   = Oid 24
+instance PqOid REGPROC where
+  pqoid REGPROC   = Oid 24
 
 -- TEXTOID
 data TEXT = TEXT
-instance PgOid TEXT where
-  pgoid TEXT   = Oid 25
+instance PqOid TEXT where
+  pqoid TEXT   = Oid 25
 
 -- OIDOID
 data OID = OID
-instance PgOid OID where
-  pgoid OID   = Oid 26
+instance PqOid OID where
+  pqoid OID   = Oid 26
 
 -- TIDOID
 data TID = TID
-instance PgOid TID where
-  pgoid TID   = Oid 27
+instance PqOid TID where
+  pqoid TID   = Oid 27
 
 -- XIDOID
 data XID = XID
-instance PgOid XID where
-  pgoid XID   = Oid 28
+instance PqOid XID where
+  pqoid XID   = Oid 28
 
 -- CIDOID
 data CID = CID
-instance PgOid CID where
-  pgoid CID   = Oid 29
+instance PqOid CID where
+  pqoid CID   = Oid 29
 
 -- OIDVECTOROID
 data OIDVECTOR = OIDVECTOR
-instance PgOid OIDVECTOR where
-  pgoid OIDVECTOR   = Oid 30
+instance PqOid OIDVECTOR where
+  pqoid OIDVECTOR   = Oid 30
 
 -- JSONOID
 data JSON = JSON
-instance PgOid JSON where
-  pgoid JSON   = Oid 114
+instance PqOid JSON where
+  pqoid JSON   = Oid 114
 
 -- XMLOID
 data XML = XML
-instance PgOid XML where
-  pgoid XML   = Oid 142
+instance PqOid XML where
+  pqoid XML   = Oid 142
 
 -- PGNODETREEOID
 data PGNODETREE = PGNODETREE
-instance PgOid PGNODETREE where
-  pgoid PGNODETREE   = Oid 194
+instance PqOid PGNODETREE where
+  pqoid PGNODETREE   = Oid 194
 
 -- POINTOID
 data POINT = POINT
-instance PgOid POINT where
-  pgoid POINT   = Oid 600
+instance PqOid POINT where
+  pqoid POINT   = Oid 600
 
 -- LSEGOID
 data LSEG = LSEG
-instance PgOid LSEG where
-  pgoid LSEG   = Oid 601
+instance PqOid LSEG where
+  pqoid LSEG   = Oid 601
 
 -- PATHOID
 data PATH = PATH
-instance PgOid PATH where
-  pgoid PATH   = Oid 602
+instance PqOid PATH where
+  pqoid PATH   = Oid 602
 
 -- BOXOID
 data BOX = BOX
-instance PgOid BOX where
-  pgoid BOX   = Oid 603
+instance PqOid BOX where
+  pqoid BOX   = Oid 603
 
 -- POLYGONOID
 data POLYGON = POLYGON
-instance PgOid POLYGON where
-  pgoid POLYGON   = Oid 604
+instance PqOid POLYGON where
+  pqoid POLYGON   = Oid 604
 
 -- LINEOID
 data LINE = LINE
-instance PgOid LINE where
-  pgoid LINE   = Oid 628
+instance PqOid LINE where
+  pqoid LINE   = Oid 628
 
 -- FLOAT4OID
 data FLOAT4 = FLOAT4
-instance PgOid FLOAT4 where
-  pgoid FLOAT4   = Oid 700
+instance PqOid FLOAT4 where
+  pqoid FLOAT4   = Oid 700
 
 -- FLOAT8OID
 data FLOAT8 = FLOAT8
-instance PgOid FLOAT8 where
-  pgoid FLOAT8   = Oid 701
+instance PqOid FLOAT8 where
+  pqoid FLOAT8   = Oid 701
 
 -- ABSTIMEOID
 data ABSTIME = ABSTIME
-instance PgOid ABSTIME where
-  pgoid ABSTIME   = Oid 702
+instance PqOid ABSTIME where
+  pqoid ABSTIME   = Oid 702
 
 -- RELTIMEOID
 data RELTIME = RELTIME
-instance PgOid RELTIME where
-  pgoid RELTIME   = Oid 703
+instance PqOid RELTIME where
+  pqoid RELTIME   = Oid 703
 
 -- TINTERVALOID
 data TINTERVAL = TINTERVAL
-instance PgOid TINTERVAL where
-  pgoid TINTERVAL   = Oid 704
+instance PqOid TINTERVAL where
+  pqoid TINTERVAL   = Oid 704
 
 -- UNKNOWNOID
 data UNKNOWN = UNKNOWN
-instance PgOid UNKNOWN where
-  pgoid UNKNOWN   = Oid 705
+instance PqOid UNKNOWN where
+  pqoid UNKNOWN   = Oid 705
 
 -- CIRCLEOID
 data CIRCLE = CIRCLE
-instance PgOid CIRCLE where
-  pgoid CIRCLE   = Oid 718
+instance PqOid CIRCLE where
+  pqoid CIRCLE   = Oid 718
 
 -- CASHOID
 data CASH = CASH
-instance PgOid CASH where
-  pgoid CASH   = Oid 790
+instance PqOid CASH where
+  pqoid CASH   = Oid 790
 
 -- MACADDROID
 data MACADDR = MACADDR
-instance PgOid MACADDR where
-  pgoid MACADDR   = Oid 829
+instance PqOid MACADDR where
+  pqoid MACADDR   = Oid 829
 
 -- INETOID
 data INET = INET
-instance PgOid INET where
-  pgoid INET   = Oid 869
+instance PqOid INET where
+  pqoid INET   = Oid 869
 
 -- CIDROID
 data CIDR = CIDR
-instance PgOid CIDR where
-  pgoid CIDR   = Oid 650
+instance PqOid CIDR where
+  pqoid CIDR   = Oid 650
 
 -- INT4ARRAYOID
 data INT4ARRAY = INT4ARRAY
-instance PgOid INT4ARRAY where
-  pgoid INT4ARRAY   = Oid 1007
+instance PqOid INT4ARRAY where
+  pqoid INT4ARRAY   = Oid 1007
 
 -- TEXTARRAYOID
 data TEXTARRAY = TEXTARRAY
-instance PgOid TEXTARRAY where
-  pgoid TEXTARRAY   = Oid 1009
+instance PqOid TEXTARRAY where
+  pqoid TEXTARRAY   = Oid 1009
 
 -- FLOAT4ARRAYOID
 data FLOAT4ARRAY = FLOAT4ARRAY
-instance PgOid FLOAT4ARRAY where
-  pgoid FLOAT4ARRAY   = Oid 1021
+instance PqOid FLOAT4ARRAY where
+  pqoid FLOAT4ARRAY   = Oid 1021
 
 -- ACLITEMOID
 data ACLITEM = ACLITEM
-instance PgOid ACLITEM where
-  pgoid ACLITEM   = Oid 1033
+instance PqOid ACLITEM where
+  pqoid ACLITEM   = Oid 1033
 
 -- CSTRINGARRAYOID
 data CSTRINGARRAY = CSTRINGARRAY
-instance PgOid CSTRINGARRAY where
-  pgoid CSTRINGARRAY   = Oid 1263
+instance PqOid CSTRINGARRAY where
+  pqoid CSTRINGARRAY   = Oid 1263
 
 -- BPCHAROID
 data BPCHAR = BPCHAR
-instance PgOid BPCHAR where
-  pgoid BPCHAR   = Oid 1042
+instance PqOid BPCHAR where
+  pqoid BPCHAR   = Oid 1042
 
 -- VARCHAROID
 data VARCHAR = VARCHAR
-instance PgOid VARCHAR where
-  pgoid VARCHAR   = Oid 1043
+instance PqOid VARCHAR where
+  pqoid VARCHAR   = Oid 1043
 
 -- DATEOID
 data DATE = DATE
-instance PgOid DATE where
-  pgoid DATE   = Oid 1082
+instance PqOid DATE where
+  pqoid DATE   = Oid 1082
 
 -- TIMEOID
 data TIME = TIME
-instance PgOid TIME where
-  pgoid TIME   = Oid 1083
+instance PqOid TIME where
+  pqoid TIME   = Oid 1083
 
 -- TIMESTAMPOID
 data TIMESTAMP = TIMESTAMP
-instance PgOid TIMESTAMP where
-  pgoid TIMESTAMP   = Oid 1114
+instance PqOid TIMESTAMP where
+  pqoid TIMESTAMP   = Oid 1114
 
 -- TIMESTAMPTZOID
 data TIMESTAMPTZ = TIMESTAMPTZ
-instance PgOid TIMESTAMPTZ where
-  pgoid TIMESTAMPTZ   = Oid 1184
+instance PqOid TIMESTAMPTZ where
+  pqoid TIMESTAMPTZ   = Oid 1184
 
 -- INTERVALOID
 data INTERVAL = INTERVAL
-instance PgOid INTERVAL where
-  pgoid INTERVAL   = Oid 1186
+instance PqOid INTERVAL where
+  pqoid INTERVAL   = Oid 1186
 
 -- TIMETZOID
 data TIMETZ = TIMETZ
-instance PgOid TIMETZ where
-  pgoid TIMETZ   = Oid 1266
+instance PqOid TIMETZ where
+  pqoid TIMETZ   = Oid 1266
 
 -- BITOID
 data BIT = BIT
-instance PgOid BIT where
-  pgoid BIT   = Oid 1560
+instance PqOid BIT where
+  pqoid BIT   = Oid 1560
 
 -- VARBITOID
 data VARBIT = VARBIT
-instance PgOid VARBIT where
-  pgoid VARBIT   = Oid 1562
+instance PqOid VARBIT where
+  pqoid VARBIT   = Oid 1562
 
 -- NUMERICOID
 data NUMERIC = NUMERIC
-instance PgOid NUMERIC where
-  pgoid NUMERIC   = Oid 1700
+instance PqOid NUMERIC where
+  pqoid NUMERIC   = Oid 1700
 
 -- REFCURSOROID
 data REFCURSOR = REFCURSOR
-instance PgOid REFCURSOR where
-  pgoid REFCURSOR   = Oid 1790
+instance PqOid REFCURSOR where
+  pqoid REFCURSOR   = Oid 1790
 
 -- REGPROCEDUREOID
 data REGPROCEDURE = REGPROCEDURE
-instance PgOid REGPROCEDURE where
-  pgoid REGPROCEDURE   = Oid 2202
+instance PqOid REGPROCEDURE where
+  pqoid REGPROCEDURE   = Oid 2202
 
 -- REGOPEROID
 data REGOPER = REGOPER
-instance PgOid REGOPER where
-  pgoid REGOPER   = Oid 2203
+instance PqOid REGOPER where
+  pqoid REGOPER   = Oid 2203
 
 -- REGOPERATOROID
 data REGOPERATOR = REGOPERATOR
-instance PgOid REGOPERATOR where
-  pgoid REGOPERATOR   = Oid 2204
+instance PqOid REGOPERATOR where
+  pqoid REGOPERATOR   = Oid 2204
 
 -- REGCLASSOID
 data REGCLASS = REGCLASS
-instance PgOid REGCLASS where
-  pgoid REGCLASS   = Oid 2205
+instance PqOid REGCLASS where
+  pqoid REGCLASS   = Oid 2205
 
 -- REGTYPEOID
 data REGTYPE = REGTYPE
-instance PgOid REGTYPE where
-  pgoid REGTYPE   = Oid 2206
+instance PqOid REGTYPE where
+  pqoid REGTYPE   = Oid 2206
 
 -- REGTYPEARRAYOID
 data REGTYPEARRAY = REGTYPEARRAY
-instance PgOid REGTYPEARRAY where
-  pgoid REGTYPEARRAY   = Oid 2211
+instance PqOid REGTYPEARRAY where
+  pqoid REGTYPEARRAY   = Oid 2211
 
 -- TSVECTOROID
 data TSVECTOR = TSVECTOR
-instance PgOid TSVECTOR where
-  pgoid TSVECTOR   = Oid 3614
+instance PqOid TSVECTOR where
+  pqoid TSVECTOR   = Oid 3614
 
 -- GTSVECTOROID
 data GTSVECTOR = GTSVECTOR
-instance PgOid GTSVECTOR where
-  pgoid GTSVECTOR   = Oid 3642
+instance PqOid GTSVECTOR where
+  pqoid GTSVECTOR   = Oid 3642
 
 -- TSQUERYOID
 data TSQUERY = TSQUERY
-instance PgOid TSQUERY where
-  pgoid TSQUERY   = Oid 3615
+instance PqOid TSQUERY where
+  pqoid TSQUERY   = Oid 3615
 
 -- REGCONFIGOID
 data REGCONFIG = REGCONFIG
-instance PgOid REGCONFIG where
-  pgoid REGCONFIG   = Oid 3734
+instance PqOid REGCONFIG where
+  pqoid REGCONFIG   = Oid 3734
 
 -- REGDICTIONARYOID
 data REGDICTIONARY = REGDICTIONARY
-instance PgOid REGDICTIONARY where
-  pgoid REGDICTIONARY   = Oid 3769
+instance PqOid REGDICTIONARY where
+  pqoid REGDICTIONARY   = Oid 3769
 
 -- INT4RANGEOID
 data INT4RANGE = INT4RANGE
-instance PgOid INT4RANGE where
-  pgoid INT4RANGE   = Oid 3904
+instance PqOid INT4RANGE where
+  pqoid INT4RANGE   = Oid 3904
 
 -- RECORDOID
 data RECORD = RECORD
-instance PgOid RECORD where
-  pgoid RECORD   = Oid 2249
+instance PqOid RECORD where
+  pqoid RECORD   = Oid 2249
 
 -- RECORDARRAYOID
 data RECORDARRAY = RECORDARRAY
-instance PgOid RECORDARRAY where
-  pgoid RECORDARRAY   = Oid 2287
+instance PqOid RECORDARRAY where
+  pqoid RECORDARRAY   = Oid 2287
 
 -- CSTRINGOID
 data CSTRING = CSTRING
-instance PgOid CSTRING where
-  pgoid CSTRING   = Oid 2275
+instance PqOid CSTRING where
+  pqoid CSTRING   = Oid 2275
 
 -- ANYOID
 data ANY = ANY
-instance PgOid ANY where
-  pgoid ANY   = Oid 2276
+instance PqOid ANY where
+  pqoid ANY   = Oid 2276
 
 -- ANYARRAYOID
 data ANYARRAY = ANYARRAY
-instance PgOid ANYARRAY where
-  pgoid ANYARRAY   = Oid 2277
+instance PqOid ANYARRAY where
+  pqoid ANYARRAY   = Oid 2277
 
 -- VOIDOID
 data VOID = VOID
-instance PgOid VOID where
-  pgoid VOID   = Oid 2278
+instance PqOid VOID where
+  pqoid VOID   = Oid 2278
 
 -- TRIGGEROID
 data TRIGGER = TRIGGER
-instance PgOid TRIGGER where
-  pgoid TRIGGER   = Oid 2279
+instance PqOid TRIGGER where
+  pqoid TRIGGER   = Oid 2279
 
 -- EVTTRIGGEROID
 data EVTTRIGGER = EVTTRIGGER
-instance PgOid EVTTRIGGER where
-  pgoid EVTTRIGGER   = Oid 3838
+instance PqOid EVTTRIGGER where
+  pqoid EVTTRIGGER   = Oid 3838
 
 -- LANGUAGE_HANDLEROID
 data LANGUAGE_HANDLER = LANGUAGE_HANDLER
-instance PgOid LANGUAGE_HANDLER where
-  pgoid LANGUAGE_HANDLER   = Oid 2280
+instance PqOid LANGUAGE_HANDLER where
+  pqoid LANGUAGE_HANDLER   = Oid 2280
 
 -- INTERNALOID
 data INTERNAL = INTERNAL
-instance PgOid INTERNAL where
-  pgoid INTERNAL   = Oid 2281
+instance PqOid INTERNAL where
+  pqoid INTERNAL   = Oid 2281
 
 -- OPAQUEOID
 data OPAQUE = OPAQUE
-instance PgOid OPAQUE where
-  pgoid OPAQUE   = Oid 2282
+instance PqOid OPAQUE where
+  pqoid OPAQUE   = Oid 2282
 
 -- ANYELEMENTOID
 data ANYELEMENT = ANYELEMENT
-instance PgOid ANYELEMENT where
-  pgoid ANYELEMENT   = Oid 2283
+instance PqOid ANYELEMENT where
+  pqoid ANYELEMENT   = Oid 2283
 
 -- ANYNONARRAYOID
 data ANYNONARRAY = ANYNONARRAY
-instance PgOid ANYNONARRAY where
-  pgoid ANYNONARRAY   = Oid 2776
+instance PqOid ANYNONARRAY where
+  pqoid ANYNONARRAY   = Oid 2776
 
 -- ANYENUMOID
 data ANYENUM = ANYENUM
-instance PgOid ANYENUM where
-  pgoid ANYENUM   = Oid 3500
+instance PqOid ANYENUM where
+  pqoid ANYENUM   = Oid 3500
 
 -- FDW_HANDLEROID
 data FDW_HANDLER = FDW_HANDLER
-instance PgOid FDW_HANDLER where
-  pgoid FDW_HANDLER   = Oid 3115
+instance PqOid FDW_HANDLER where
+  pqoid FDW_HANDLER   = Oid 3115
 
 -- ANYRANGEOID
 data ANYRANGE = ANYRANGE
-instance PgOid ANYRANGE where
-  pgoid ANYRANGE   = Oid 3831
+instance PqOid ANYRANGE where
+  pqoid ANYRANGE   = Oid 3831
 
 -- UUID
 data UUID = UUID
-instance PgOid UUID where
-  pgoid UUID   = Oid 2950
+instance PqOid UUID where
+  pqoid UUID   = Oid 2950
