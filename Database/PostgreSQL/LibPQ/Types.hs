80147a3bdb0c8ead4a0243f0f5a5cf1a841c0cbb
Kirk Peterson <necrobious@gmail.com>
2012-09-17 10:04:17 -0700

    LibPQ class signature changes

--- Database/PostgreSQL/LibPQ/Types.hs
+++ Database/PostgreSQL/LibPQ/Types.hs
@@ -20,8 +20,8 @@ class PqOid oidtype where
 -- A Typeclass for marking a type a having the ability to be transformed into and out of LibPQ values.
 class LibPQ a where
   oidPQ  :: a -> Oid 
-  toPQ   :: a -> Maybe (Oid, ByteString, Format)
-  fromPQ :: Maybe ByteString -> Maybe a
+  toPQ   :: a -> Maybe (ByteString, Format)
+  fromPQ :: Maybe (ByteString, Format) -> Maybe a
 
 -- BOOLOID
 data BOOL = BOOL
